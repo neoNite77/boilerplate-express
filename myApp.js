78c377e99696d9bee9fda18f452752f9e499672e
neoNite77 <abrahamdaungos@gmail.com>
2022-12-21 21:12:13 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,9 +3,20 @@ let app = express();
 
 console.log("Hello World");
 
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+//   });
+
 app.get("/", (req, res) => {
-    res.send("Hello Express");
-  });
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Normal usage
+// app.use(express.static(__dirname + "/public"));
+
+// Can now serve files in the public directory
+// Usage where assests are at the /public route (public directory to public path)
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
@@ -38,5 +49,4 @@ app.get("/", (req, res) => {
 
 
 
-
- module.exports = app;
+module.exports = app;
