f9bd7542a4035e2ace663302fc231eae7bf29f7b
neoNite77 <abrahamdaungos@gmail.com>
2022-12-22 21:47:18 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const e = require('express');
 let express = require('express');
 let app = express();
 
@@ -17,6 +19,18 @@ app.get("/", (req, res) => {
 // Can now serve files in the public directory
 // Usage where assests are at the /public route (public directory to public path)
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({
+      "message": "HELLO JSON"
+    })
+  } else {
+    res.json({
+      "message": "Hello json"
+    })
+  }
+});
 
 
 
